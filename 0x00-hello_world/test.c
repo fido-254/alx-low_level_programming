aac1a4d8eaeaef4c0a399f682ba83a9955631ca7
Stephen <osteve964@gmail.com>
2022-10-14 15:08:41 -0700

    While Statements

---
+++ 0x00-hello_world/test.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ *
+ * main - start of the code
+ *
+ * Description: A C programme that tests betty functions
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+
+{
+	int a, b;
+
+	a = 20;
+	b = 10;
+	
+	if a > b
+	{
+		return (a);
+	}
+}

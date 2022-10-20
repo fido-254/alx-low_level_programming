e40a7af4dead6fb45cb18bccde1247eaebe1106b
Stephen <osteve964@gmail.com>
2022-10-19 17:27:27 -0700

    Main

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,9 @@
+int _putchar(char);
+int print_alphabet();
+int print_alphabet_x10();
+int _islower(int);
+int _isalpha(int);
+int print_sign(int);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);

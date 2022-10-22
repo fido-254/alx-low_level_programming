7ad3f7410307082c2b7161727226b4d5768ffaee
Stephen <osteve964@gmail.com>
2022-10-21 18:08:21 -0700

    Fuzzy Hotfix

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,24 +11,29 @@
 
 int main(void)
 {
-	int num;
+	int cont = 1;
 
-	for (num = 1; num <= 100; ++num)
+	printf("%d", cont);
+	cont++;
+	for (; cont <= 100; cont++)
 	{
-		if (num % 3 == 0 && !(num % 5 == 0))
-			printf("Fizz");
-		else if (num % 5 == 0 && !(num % 3 == 0))
-			printf("Buzz");
-		else if (num % 3 == 0 && num % 5 == 0)
-			printf("FizzBuzz");
+		if (cont % 3 == 0 && cont % 5 == 0)
+		{
+			printf(" FizzBuzz");
+		}
+		else if (cont % 3 == 0)
+		{
+			printf(" Fizz");
+		}
+		else if (cont % 5 == 0)
+		{
+			printf(" Buzz");
+		}
 		else
-			printf("%d", num);
-
-		if (num != 100)
-			printf(" ");
-		else
-			printf("\n");
+		{
+			printf(" %d", cont);
+		}
 	}
-
+	printf("\n");
 	return (0);
 }

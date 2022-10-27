a5b1150f5c1b865fb025205e09bce240819ca084
Stephen <osteve964@gmail.com>
2022-10-27 12:37:47 -0700

    Pointers

---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,12 @@
+void _putchar(int);
+char *_strcat(char *, char *);
+char *_strncat(char *, char *, int);
+char *_strncpy(char *, char *, int n);
+int _strcmp(char *, char *);
+void reverse_array(int *, int);
+char *string_toupper(char *);
+char *cap_string(char *);
+char *leet(char *);
+char *rot13(char *);
+void print_number(int);
+char *infinite_add(char *, char *, char *, int);

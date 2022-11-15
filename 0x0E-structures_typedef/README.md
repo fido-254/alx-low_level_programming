5dd6ce91a348477a9fc28c3121a8f0a8f68b3ae9
Stephen <osteve964@gmail.com>
2022-11-14 16:09:06 -0800

    Good Boy

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+Struct more of Them
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,27 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+
+/**
+ * struct dog - dog
+ * @name: Minion
+ * @age: 3
+ * @owner: Max
+ *
+ * Description: Stuart The Dog
+ *
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif
